2a87768ac437c384ef0b7de53ca02f80df8a28dc
Natalie <nroberts@radiantblue.com>
2017-10-03 16:22:58 -0400

    Uncommented out hystrix stuff.

--- apps/omar-download-app/grails-app/init/download/app/Application.groovy
+++ apps/omar-download-app/grails-app/init/download/app/Application.groovy
@@ -4,11 +4,11 @@ import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
-//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
-//@EnableCircuitBreaker
-//@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 
 class Application extends GrailsAutoConfiguration {
