b0c35ae7cbb4c1e095cc82dbdaf5a470f7b0b9a0
Natalie <nroberts@radiantblue.com>
2017-10-03 17:41:44 -0400

    Enabled hystrix instead of hystrixdashboard.

--- apps/omar-download-app/grails-app/init/download/app/Application.groovy
+++ apps/omar-download-app/grails-app/init/download/app/Application.groovy
@@ -3,12 +3,11 @@ package download.app
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
 
+@EnableHystrix
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 @EnableDiscoveryClient
 
 class Application extends GrailsAutoConfiguration {
