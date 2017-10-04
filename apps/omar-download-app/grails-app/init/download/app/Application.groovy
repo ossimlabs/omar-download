941917ee7a8b6e6bb12df000308eca301591930a
Natalie <nroberts@radiantblue.com>
2017-10-04 14:41:30 -0400

    Added HystrixCommand fallback.

--- apps/omar-download-app/grails-app/init/download/app/Application.groovy
+++ apps/omar-download-app/grails-app/init/download/app/Application.groovy
@@ -6,6 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.netflix.hystrix.EnableHystrix
 
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+import com.netflix.appinfo.InstanceInfo.InstanceStatus
+
 @EnableHystrix
 @EnableCircuitBreaker
 @EnableDiscoveryClient
@@ -14,4 +18,15 @@ class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
+
+    @HystrixCommand(fallbackMethod = "statusNotFound")
+    public InstanceStatus notificationsStatus() {
+        return discoveryClient.getNextServerFromEureka("OMAR-DOWNLOAD", false)
+                .getStatus();
+    }
+
+    public InstanceStatus statusNotFound() {
+        return InstanceStatus.DOWN;
+    }
+
 }
