6216e7010c43319ccf5572222bce0874f44237a5
Natalie <nroberts@radiantblue.com>
2017-10-05 14:48:33 -0400

    Testing Hystrix stream

--- plugins/omar-download-plugin/grails-app/init/omar/download/Application.groovy
+++ plugins/omar-download-plugin/grails-app/init/omar/download/Application.groovy
@@ -5,7 +5,9 @@ import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.*
 
 import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
+@EnableHystrixDashboard
 @EnableHystrix
 @PluginSource
 class Application extends GrailsAutoConfiguration {
