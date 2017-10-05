99621dd898a9b17f6c9f9678c848e1e158dcff86
Natalie <nroberts@radiantblue.com>
2017-10-05 14:57:12 -0400

    Hystrix stream test.

--- apps/omar-download-app/grails-app/init/download/app/Application.groovy
+++ apps/omar-download-app/grails-app/init/download/app/Application.groovy
@@ -4,6 +4,11 @@ import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+
+@EnableHystrixDashboard
+@EnableHystrix
 @EnableDiscoveryClient
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
