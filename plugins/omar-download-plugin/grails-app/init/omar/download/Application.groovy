763e5fea8508a77e4b85b63552c2f87a1bd9f74f
Natalie <nroberts@radiantblue.com>
2017-10-19 23:48:57 -0400

    Fixed bug that would cause download to fail when hystrix is enabled.

--- plugins/omar-download-plugin/grails-app/init/omar/download/Application.groovy
+++ plugins/omar-download-plugin/grails-app/init/omar/download/Application.groovy
@@ -3,9 +3,7 @@ package omar.download
 import grails.boot.*
 import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.*
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 
-@EnableCircuitBreaker
 @PluginSource
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
