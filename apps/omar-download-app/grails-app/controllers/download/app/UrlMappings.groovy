9c60d6271eaa4839746df1129da4b64f7dd4d99a
Aaron Drake <melbsurfer@gmail.com>
2018-12-03 12:55:40 -0500

    Testing build

--- apps/omar-download-app/grails-app/controllers/download/app/UrlMappings.groovy
+++ apps/omar-download-app/grails-app/controllers/download/app/UrlMappings.groovy
@@ -5,7 +5,7 @@ class UrlMappings {
     static mappings = {
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
-                // apply constraints here
+                // apply constraints here!
             }
         }
 
